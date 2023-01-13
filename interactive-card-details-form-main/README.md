48b0488e4005d242c0bb38782e8de9ecf8f54ecf
RechainPath <abv4594@gmail.com>
2023-01-13 22:39:59 +0200

    Project done for Desktop

--- interactive-card-details-form-main/README.md
+++ interactive-card-details-form-main/README.md
@@ -1,16 +1,47 @@
-# Frontend Mentor - Interactive card details form (Front End Mentor)
+# Frontend Mentor - Interactive card details form solution
 
-**Project done for learning purposes**
+This is a solution to the [Interactive card details form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-## Partial Solution (development ongoing)
+## Table of contents
 
-### What is done:
-- Form with interactivity and validation. I took the approach to display the error messages when user change focus.
-- There is big potential to improve the code.
+### The challenge
 
-### What is missing:
-- Confirmation button action plus confirmation page itself 
+Users should be able to:
 
-### Challenges ahead:
-- Study the functioning of the button + display of confirmation page
-- Study how to clear and reset
+- Fill in the form and see the card details update in real-time
+- Receive error messages when the form is submitted if:
+  - Any input field is empty
+  - The card number, expiry date, or CVC fields are in the wrong format
+- View the optimal layout depending on their device's screen size
+- See hover, active, and focus states for interactive elements on the page
+
+### Links
+
+- Solution URL: [GitHub] (https://github.com/abv4594/frontend-mentor/tree/main/interactive-card-details-form-main)
+- Live Site URL: [Solution](https://jolly-mochi-4fa7e8.netlify.app/)
+
+### Built with
+
+- HTML
+- CSS
+- CSS Flexbox & Grid
+- Javascript
+
+### What I learned
+
+- Great to review DOM manipulation. I didn't plan in advance and started to develop the project. During the development I noticed there was space to optimize, if I had planned in advance. Some aspects I implemented in the code which I believe are smart (not sure if they are recommended though):
+
+- Relate IDs from the input fields in the form with the ids in the (dummy) card and the ids of the errors (which apppear below the input fields)
+    - For instance, the input field for the card number has the id "cardNumber". The id of the paragraph containing the same field in the card is "cardNumberInCard"
+    - So faciliatates code reutilization. 
+
+- Use functions with the same name as ids and use ```window[id]``` to call the function.
+    - This also helped to optimize the code. For instance, to validate the card number, with ```id=cardNumber```, there is a function ```cardNumber()```. 
+
+### Useful resources
+
+- [This codepen](https://codepen.io/P1xt/pen/wvxddQV?editors=1010) - was a great a recap of DOM and of great help to me. 
+
+
+
+
--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -5,13 +5,17 @@
 //or the id of the error message)
 
 // some global constants and objects
+
 let defaultValues = {}; // store defult values
-const cardNumberSpaces = [4,9,14]; // used to make the space effect in the card number
+const cardNumberSpaces = [4,9,14]; // space effect in card number
+let formUsed = false; // control if inputed text in form
+
 
 
 // initial set up, add event listeners and collect default values (from HTML)
 
 window.addEventListener('DOMContentLoaded', (event)=> {
+
 
     const entryInputs = document.getElementsByClassName('entry-input');
     for (const entryInput of entryInputs) {
@@ -22,11 +26,49 @@ window.addEventListener('DOMContentLoaded', (event)=> {
         defaultValues[entryInput.id] = entryInput.value;
     }
 
+    const confirmBtn = document.getElementById('confirm-btn');
+    confirmBtn.addEventListener('click', toggleDisplay);
+
+    const continueBtn = document.getElementById('continue-btn');
+    continueBtn.addEventListener('click', toggleDisplay);
 
 })
 
 
+function toggleDisplay(e) {
+    e.preventDefault();
+    const idBtn = e.target.id;
+    if (idBtn === 'confirm-btn') {
+        // make form disappear
+        if (!validateInputs()) return; // if fields not valid igonre click
+        document.getElementsByClassName('inputForm')[0].style.display = "none";
+
+        // make thank you appear
+        const thankYouDiv = document.getElementsByClassName('thankYou')[0];
+        thankYouDiv.style.display = "flex";
+        thankYouDiv.style.flexDirection = "column";
+        thankYouDiv.style.alignItems = "center";
+    } else {
+        // make thank you disappear
+        const thankYouDiv = document.getElementsByClassName('thankYou')[0];
+        thankYouDiv.style.display = "none";
+
+        // reset default value
+        resetPage();
+
+        // make form appear again
+        document.getElementsByClassName('inputForm')[0].style.display = "flex";
+        
+        
+
+
+    }
+}
+
+
+
 function updateCard(e) {
+    formUsed = true;
     const inCardEl = findInCardEl(e.target.id, "InCard");
 
     // add spaces effect
@@ -45,7 +87,8 @@ function entryFocus(e) {
     //check if there is currently data in the input field
     if (e.target.value === defaultValues[e.target.id]) {
         e.target.value = "";
-        e.target.style.color = 'hsl(278, 68%, 11%)';
+        //e.target.style.color = 'hsl(278, 68%, 11%)';
+        toggleColors(e.target);
     } 
 }
 
@@ -54,28 +97,31 @@ function entryBlur(e) {
     // revert to default values in case user clean up the inputs:
     if (e.target.value === "") {
         e.target.value = defaultValues[e.target.id];
-        e.target.style.color = 'hsl(270, 3%, 87%)';
+        //e.target.style.color = 'hsl(270, 3%, 87%)';
+        toggleColors(e.target);
         inCardEl = findInCardEl(e.target.id, "InCard");
         inCardEl.textContent = defaultValues[inCardEl.id];
         return // no need to continue
     }
 
     //validate entry:
-    if (!window[e.target.id](e)) { // will call a function with the same name of id
-        displayError(e);
+    if (!window[e.target.id](e.target)) { // will call a function with the same name of id
+        displayError(e.target);
     } else {
-        cleanError(e);
+        cleanError(e.target);
     }
 }
 
 // validation functions (function name = id of the element)
 
 function cardHolderName(el) {
-    return true // you can add whatever as a name
+    if (!hasLetters(el.value)) return false;
+    return true
 }
 
 function mm(el) {
-    const month = el.target.value;
+    const month = el.value;
+    if (month === defaultValues[el.id]) return false; 
     if (month.length !== 2) return false;
     if (hasLetters(month)) return false;
     if (parseInt(month) < 1 || parseInt(month) > 12) return false;
@@ -83,7 +129,8 @@ function mm(el) {
 }
 
 function yy(el) {
-    const year = el.target.value;
+    const year = el.value;
+    if (year === defaultValues[el.id]) return false;
     if (year.length !== 2) return false;
     if (hasLetters(year)) return false;
     if (parseInt(year) < 22) return false;
@@ -91,7 +138,8 @@ function yy(el) {
 }
 
 function cvv(el) {
-    const cvvNum = el.target.value;
+    const cvvNum = el.value;
+    if (cvvNum === defaultValues[el.id]) return false
     if (cvvNum.length !== 3) return false;
     if (hasLetters(cvvNum)) return false;
     return true;
@@ -99,24 +147,21 @@ function cvv(el) {
 
 function cardNumber(el) {
     // validating card number
-    const cardNumberNoSpace = el.target.value.split(" ").join("");
+    const cardNumberNoSpace = el.value.split(" ").join("");
     if (cardNumberNoSpace.length !== 16) return false;
     if (hasLetters(cardNumberNoSpace)) return false;
     return true;
 }
 
-
-
-
 // functions to control errors display
 function cleanError(el) {
-    const elErr = findInCardEl(el.target.id, "Err");
-    el.target.style.border = "1px solid hsl(270, 3%, 87%)"
+    const elErr = findInCardEl(el.id, "Err");
+    el.style.border = "1px solid hsl(270, 3%, 87%)"
     elErr.style.color = "white";
 }
 function displayError(el) {
-    const errEl = findInCardEl(el.target.id,"Err");
-    el.target.style.border = "1px solid red"
+    const errEl = findInCardEl(el.id,"Err");
+    el.style.border = "1px solid red"
     errEl.style.color = "red";
 }
 
@@ -128,4 +173,59 @@ function hasLetters(str) {
 
 function findInCardEl (id, suffix) {
     return document.getElementById(id + suffix);
+}
+
+
+function validateInputs() {
+    const inputs = document.getElementsByClassName('entry-input');
+    let formValid = true;
+    for (const el of inputs) {
+        if (!window[el.id](el)) {
+            displayError(el);   
+            formValid = false;
+        }
+    }
+    return formValid;
+}
+
+/*function validateInputs() {
+    //get all error messages
+    const errs = document.querySelectorAll("[class^=err]");
+    // if any is red return 
+    for (const el of errs) {
+        if (el.style.color === 'red') return false;
+    }
+
+    // used pressed Confirm without doing nothing
+    if (!formUsed) return false;
+
+    // special case: user presses confirm without doing nothing
+    //const inputs = document.querySelectorAll(".entry-input");
+    //if ([...inputs].every(field => field.value === defaultValues[field.id])) return false;
+
+    return true;
+}*/
+
+function resetPage() {
+
+    formUsed = false 
+
+    for (const id in defaultValues) {
+        const el = document.getElementById(id);
+        
+        // if el is in the card, updates textContent:
+        if (el.id.includes("InCard")) {
+            el.textContent = defaultValues[id];
+        // else updates value
+        } else {
+            el.value = defaultValues[id];
+            toggleColors(el);
+         
+        }
+    } 
+} 
+
+function toggleColors(el) {
+    el.classList.toggle('fontDark');
+    el.classList.toggle('fontClear');
 }
